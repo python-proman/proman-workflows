fe74c52fad186d86fd57792aed4b1e9287536903
Jesse P. Johnson <jpj6652@gmail.com>
2020-10-05 16:38:21 -0400

    Implemented title and body parser

--- git_tools/message.py
+++ git_tools/message.py
@@ -6,18 +6,43 @@
 import logging
 import os
 
-from lark import Lark, logger
+from lark import Lark, logger, Transformer
 
 grammar_path = os.path.join('git_tools', 'grammars', 'conventional_commits.lark')
 
-message = '''fix(example): test a message
+message = '''fix(example)!: test a message
 
 test body of comment test
 test body of comment 2 test
 
+Reviewed-by: Jesse <jpj6652@gmail.com>
 Refs #123
+Refs #124
 BREAKING CHANGE: This could change things
 '''
 
+class MessageTransformer(Transformer):
+    def title(self, args):
+        title = {}
+        for arg in args:
+            title[arg.data] = next((x for x in arg.children), True)
+        return title
+
+    def body(self, args=None):
+        return [arg.value for arg in args]
+
+    def footer(self, args=None):
+        footer = {}
+        for arg in args:
+            if arg.data == 'trailer':
+                footer['trailer'] = {}
+                for x in arg.children:
+                    print(x.data, [arg for arg in args][0])
+                    # footer['trailer'][x.data] = [arg for arg in args][0]
+        print('footer:', footer)
+        return args
+
 parser = Lark.open(grammar_path, start='message', debug=True)
 print(parser.parse(message).pretty())
+tree = parser.parse(message)
+MessageTransformer().transform(tree=tree)
