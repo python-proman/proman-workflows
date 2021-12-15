a66a396ffe14c9c90ed0bb901ee4a79a5b72aab5
Jesse P. Johnson <jpj6652@gmail.com>
2021-12-15 17:54:18 -0500

    ci(version): apply 0.1.0a4 updates

--- src/proman_workflows/__init__.py
+++ src/proman_workflows/__init__.py
@@ -34,7 +34,7 @@ __author__ = 'Jesse P. Johnson'
 __author_email__ = 'jpj6652@gmail.com'
 __title__ = 'proman_workflows'
 __description__ = 'Convenience module to manage project tools with Python.'
-__version__ = '0.1.0a3'
+__version__ = '0.1.0a4'
 __license__ = 'MPL-2.0'
 __copyright__ = 'Copyright 2021 Jesse Johnson.'
 
