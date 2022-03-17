6f7bb33d9cdec2cd16a123bebea2c9b594602193
Jesse P. Johnson <jpj6652@gmail.com>
2022-03-16 22:23:11 -0400

    ci: remove examples

--- examples/letter.py
+++
@@ -1,7 +0,0 @@
-from packaging.version import _parse_letter_version, _parse_local_version
-
-v1 = _parse_letter_version('alpha', 2)
-print(type(v1), v1)
-
-v2 = _parse_local_version('build.4')
-print(type(v2), v2)
--- examples/mock_workflow/__main__.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-# copyright: (c) 2020 by Jesse Johnson.
-# license: MPL-2.0, see LICENSE for more details.
-"""Provide CLI management."""
-
-from mock_workflow import workflow
-
-if __name__ == '__main__':
-    workflow.run()
